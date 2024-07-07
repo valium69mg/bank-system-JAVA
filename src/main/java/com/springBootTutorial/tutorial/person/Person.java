406d4c3a0db53ce9996c57fa6eefdbe498eb5c76
valium69mg <carlostranquilino.cr@gmail.com>
2024-07-07 04:02:50 -0600

    Add a constructor method to construct Person class with all its attributes and removed Address class from Person class file

--- src/main/java/com/springBootTutorial/tutorial/person/Person.java
+++ src/main/java/com/springBootTutorial/tutorial/person/Person.java
@@ -3,145 +3,7 @@ package com.springBootTutorial.tutorial.person;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-class Address {
-	
-	private Integer personId;
-	private String streetName;
-	private Integer streetNumber;
-	private Integer apartmentOrUnitNumber;
-	private Integer zipCode;
-	private String city;
-	private String state;
-	private String country;
-	
-	public Address(Integer personId) {
-		this.personId = personId;
-	}
-	
-	public HashMap<String,String> getAddressInfo() throws Exception { 
-		HashMap<String, String> addressInfo = new HashMap<String, String>();
-		//CHECK FOR UNINITIALIZED ATTRIBUTES
-		if (streetName == null) {
-			throw new Exception("An exception occurred: Street Name in Address Class is null, must be initialized first");
 
-		}
-		addressInfo.put("streetName", streetName);
-		if (streetNumber == null) {
-			throw new Exception("An exception occurred: Street Number in Address Class is null, must be initialized first");
-		}
-		addressInfo.put("streetNumber", streetNumber.toString());
-		if (zipCode == null) {
-			throw new Exception("An exception occurred: Zip Code in Address Class is null, must be initialized first");
-		}
-		addressInfo.put("zipCode", zipCode.toString());
-		if (city == null) {
-			throw new Exception("An exception occurred: City in Address Class is null, must be initialized first");
-		}
-		addressInfo.put("city", city);
-		if (state == null) {
-			throw new Exception("An exception occurred: State in Address Class is null, must be initialized first");
-
-		}
-		addressInfo.put("state", state);
-		if (country == null) {
-			throw new Exception("An exception occurred: Country in Address Class is null, must be initialized first");
-		}
-		addressInfo.put("country", country);
-		
- 		if (apartmentOrUnitNumber == null) {
- 			return addressInfo;
- 		}
- 		
- 		addressInfo.put("apartmentOrUnitNumber", apartmentOrUnitNumber.toString());
- 		return addressInfo;
-		
-	}
-	
-	// GETTERS AND SETTERS
-	
-	// PERSON ID
-	public Integer getPersonId() {
-		return personId;
-	}
-	
-	// STREET NAME
-	public void setStreetName(String streetName) {
-		this.streetName = streetName;
-	}
-	
-	public String getStreetName() {
-		return streetName;
-	}
-	
-	// STREET NUMBER
-	public void setStreetNumber(Integer streetNumber) {
-		this.streetNumber = streetNumber;
-	}
-	
-	public Integer getStreetNumber() {
-		return streetNumber;
-	}
-	
-	// APARTMENT OR UNIT NUMBER
-	public void setApartmentOrUnitNumber(Integer apartmentOrUnitNumber) {
-		this.apartmentOrUnitNumber = apartmentOrUnitNumber; 
-	}
-	
-	public Integer getApartmentOrUnitNumber() {
-		return apartmentOrUnitNumber;
-	}
-	
-	// ZIP CODE
-	public void setZipCode(Integer zipCode) {
-		this.zipCode = zipCode;
-	}
-	
-	public Integer getZipCode() {
-		return zipCode;
-	}
-	
-	// CITY
-	public void setCity(String city) {
-		this.city = city;
-	}
-	
-	public String getCity() {
-		return city;
-	}
-	
-	// STATE
-	public void setState(String state) {
-		this.state = state;
-	}
-	
-	public String getState() {
-		return state;
-	}
-	
-	// COUNTRY 
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	
-	public String getCountry() {
-		return country;
-	}
-	
-	public void autoFillForTest() {
-		// ADDRESS ATTRIBUTES
-		this.streetName = "La Vista Av";
-		this.streetNumber = 1997;
-		this.apartmentOrUnitNumber = 0;
-		this.zipCode = 10239;
-		this.city = "Fresnillo";
-		this.state = "Zacatecas";
-		this.country = "Mexico";
-	}
-}
-
-/**
- * 
- */
 public class Person {
 	
 	private Integer personId;
@@ -155,6 +17,15 @@ public class Person {
 	public Person(Integer personId) {
 		this.personId = personId;
 		this.address = new Address(this.personId);
+	}
+	
+	public Person(Integer personId,String firstName, String lastName, String dateOfBirth, String email, Address address,String taxId) {
+		this.personId = personId;
+		this.firstName = firstName;
+		this.dateOfBirth = dateOfBirth;
+		this.email = email;
+		this.address = address;
+		this.taxId = taxId;
 	}
 	
 	
@@ -253,6 +124,10 @@ public class Person {
 
 	public Address getAddress() {
 		return address;
+	}
+	
+	public void setAddress(Address newAddress) {
+		this.address = newAddress;
 	}
 	
 	// TAX ID
